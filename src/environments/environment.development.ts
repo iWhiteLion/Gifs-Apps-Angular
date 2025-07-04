b1b3509dc8eb9c9c47115c30f6aced7e2b74e26a
Tobias Emanuel Tello Illescas <2905035@tcs.com>
2025-07-04 09:49:12 -0500

    Consumo de la API de Giphy

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+    production: true,
+    companyName: 'Gifs',
+    companyName2: 'App',
+    companySlogan: 'Maneja tus Gifs',
+
+
+    //API key
+    giphyApiKey: '5AJSHTIDgJuhkeRL8acnnXpwOCAeBMEi',
+    giphyApiUrl: 'https://api.giphy.com/v1',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+
+    production: true,
+    companyName: 'Gifs',
+    companyName2: 'App',
+    companySlogan: 'Maneja tus Gifs',
+
+
+    //API key
+    giphyApiKey: '5AJSHTIDgJuhkeRL8acnnXpwOCAeBMEi',
+    giphyApiUrl: 'https://api.giphy.com/v1',
+
+};
